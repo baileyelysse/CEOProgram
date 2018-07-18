2fd13bd977b62797142fd71a6f9b3cab7fa9542b
baileyelysse <bailey.patterson@gmail.com>
2018-07-18 15:03:35 -0500

    added string file

---
+++ strings.py
@@ -0,0 +1,15 @@
+"""
+title: strings
+author: Bailey Patterson
+date: 7/18/18 2:54 PM
+"""
+
+
+def how_eligible(essay):
+    # Returns how many categories are in a string
+
+
+
+if __name__ == '__main__':
+    print(how_eligible('This? "Yes." No, not really!'))
+    print(how_eligible('Really, not a compound sentence.'))
